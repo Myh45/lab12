fd446b11949fb28e368dc503bf47d4437e2f0e0d
Myh45 <54940009+Myh45@users.noreply.github.com>
2020-04-29 13:58:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ var rez2 = document.querySelector('.rez2');
 
 var button_calc=document.querySelector('.calc');
 var button_calc1=document.querySelector('.calc1');
+var button_calc2=document.querySelector('.calc2');
+
 
 var text='';
 var text1='';
